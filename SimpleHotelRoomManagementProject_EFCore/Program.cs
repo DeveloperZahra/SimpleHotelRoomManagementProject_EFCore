bff03394a3fad0dfc074f80f505e9772ad8bc727
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-07 11:04:15 +0400

    Add project files.

---
+++ SimpleHotelRoomManagementProject_EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleHotelRoomManagementProject_EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
