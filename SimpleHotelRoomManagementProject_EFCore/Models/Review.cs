c3aac5072b39fb5ad76e60d671e709f4ecd0a727
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-10 09:20:26 +0400

    Represents a guest review for a specific booking in the hotel management system. Includes validation rules using Data Annotations for EF Core.

--- SimpleHotelRoomManagementProject_EFCore/Models/Review.cs
+++ SimpleHotelRoomManagementProject_EFCore/Models/Review.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleHotelRoomManagementProject_EFCore.Models
 {
-    internal class Review
+    // Represents a guest review for a specific booking in the hotel management system.
+    // Includes validation rules using Data Annotations for EF Core.
+    public class Review
     {
+        [Key] // Primary key
+        public int ReviewId { get; set; }
+
+        [Required] // Must be linked to a booking
+        [ForeignKey("Booking")]
+        public int BookingId { get; set; }
+
+        [Required] // Must be linked to a guest
+        [ForeignKey("Guest")]
+        public int GuestId { get; set; }
+
+        [Required] // Review Comment  is mandatory
+        [MaxLength(500)] // Limit text length to 500 characters
+        public string Comment { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; } // Optional star rating
+
+        [Required] // Review date is mandatory
+        public DateTime ReviewDate { get; set; } = DateTime.Now;
+
     }
 }
