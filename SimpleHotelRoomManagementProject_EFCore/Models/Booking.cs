eaf8ac1ec5203ca4d3d9b25ea7e52d10f6eb1b70
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-10 10:45:14 +0400

    Represents a booking made by a guest for a specific room within a given period.

---
+++ SimpleHotelRoomManagementProject_EFCore/Models/Booking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Models
+{
+    // Represents a booking made by a guest for a specific room within a given period.
+    public class Booking
+    {
+        [Key]
+        public int BookingId { get; set; }
+
+        [Required]
+        public int GuestId { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required]
+        public DateTime CheckInDate { get; set; }
+
+        [Required]
+        public DateTime CheckOutDate { get; set; }
+
+        // Number of nights stayed
+        [NotMapped]  // This tells EF not to map this property to the database, calculated dynamically
+        public int Nights
+        {
+            get
+            {
+                return (CheckOutDate.Date - CheckInDate.Date).Days;
+            }
+        }
+
+        // Total cost of booking
+        [Column(TypeName = "decimal(18,2)")]  // Define precision for currency in DB
+        public decimal TotalCost { get; set; }
+    }
+}
