3d763bcb273c38d53e8dd57932692158aef48c7a
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-10 08:52:36 +0400

    Represents a guest in the hotel management system. Includes validation rules using Data Annotations for EF Core mapping.

---
+++ SimpleHotelRoomManagementProject_EFCore/Models/Guest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Models
+{
+    // Represents a guest in the hotel management system.
+    // Includes validation rules using Data Annotations for EF Core mapping.
+    public class Guest
+    {
+        [Key]
+        public int GuestId { get; set; }
+
+        [Required]
+        public string GuestName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
+        public string GuestEmail { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{8}$")]
+        public string GuestPhoneNumber { get; set; }
+    }
+}
