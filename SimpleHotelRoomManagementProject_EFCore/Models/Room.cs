fb924424299a4c2986118c1d981ec9e6fa78f3e9
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-10 08:20:29 +0400

    Represents a hotel room entity in the system. Uses Data Annotations for EF Core mapping.

---
+++ SimpleHotelRoomManagementProject_EFCore/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Models
+{
+    // Represents a hotel room entity in the system.
+    // Uses Data Annotations for EF Core mapping.
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+        public int RoomNumber { get; set; }
+
+        [Required]
+        public decimal DailyRate { get; set; }
+
+        public bool IsReserved { get; set; }
+
+    }
+}
