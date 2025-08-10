3b2854cac9abd8c1856c3436c282b1bf391f2a32
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-10 11:41:19 +0400

    Add a new guest to the database

--- SimpleHotelRoomManagementProject_EFCore/Repositories/GuestRepo.cs
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/GuestRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace SimpleHotelRoomManagementProject_EFCore.Repositories
 {
-    internal class GuestRepo
+    public  class GuestRepo
     {
+        private readonly HotelDbContext _context;
+        public GuestRepo(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // Add a new guest to the database
+        public void AddGuest(Guest guest)
+        {
+            _context.Guest.Add(guest); // Adds the new guest to the context
+            _context.SaveChanges(); // Saves changes to the database
+        }
+
+
+
+
     }
 }
