c0fd420ef026e91b1303c4284a07972da92f24e8
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 08:43:12 +0400

    Save booking

--- SimpleHotelRoomManagementProject_EFCore/Services/BookingServices.cs
+++ SimpleHotelRoomManagementProject_EFCore/Services/BookingServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+using SimpleHotelRoomManagementProject_EFCore.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,74 @@ using System.Threading.Tasks;
 
 namespace SimpleHotelRoomManagementProject_EFCore.Services
 {
-    internal class BookingServices
+    public class BookingServices
     {
-    }
+        private readonly IBookingRepo _bookingRepository;
+        public BookingServices(IBookingRepo bookingRepository)
+        {
+            _bookingRepository = bookingRepository ?? throw new ArgumentNullException(nameof(bookingRepository));
+        }
+
+
+        private readonly IGuestRepo _GuestRepo;
+        public BookingServices(IGuestRepo guestRepository)
+        {
+            _GuestRepo = guestRepository ?? throw new ArgumentNullException(nameof(guestRepository));
+        }
+
+
+        private readonly IRoomRepo _roomRepository;
+        public BookingServices(IRoomRepo roomRepository)
+        {
+            _roomRepository = roomRepository ?? throw new ArgumentNullException(nameof(roomRepository));
+        }
+
+        public (bool Ok, string Message, int? BookingId) AddNewBooking(int BookingId, int nights, DateTime checkInDate, int guestId, int roomId, decimal TotalCost , string Status)
+        {
+            // 1) validation for number of night
+            if (nights <= 0) return (false, "Number of nights must be greater than 0.", null);
+
+            checkInDate = checkInDate.Date; // normalize to date (00:00)
+            var checkOutDate = checkInDate.AddDays(nights);
+
+            // 2) check if room or guest exist
+            var room = _roomRepository.GetRoomById(roomId);
+            if (room == null) return (false, "There is no room with this id number.", null);
+
+            var guest = _GuestRepo.GetGuestById(guestId);
+            if (guest == null) return (false, "There is no guest with this id number.", null);
+
+            // 3) avoid booking same room in same date this state named overlaps 
+            // Overlaps rule: (startA < endB) && (endA > startB)
+            bool overlaps = _bookingRepository.ExistsOverlap(
+                roomId, checkInDate, checkOutDate);
+
+            if (overlaps)
+                return (false, "Room is not available in the selected dates.", null);
+
+
+
+            // 5) Calculate total cost
+            decimal totalCost = room.PricePerNight * nights;
+
+            // 6) Create booking object
+            var booking = new Booking
+            {
+                Nights = nights,
+                CheckInDate = checkInDate,
+                CheckOutDate = checkOutDate,
+                GuestId = guestId,
+                RoomId = roomId,
+                TotalCost = totalCost,
+                Status = "Confirmed"
+            };
+
+            // 7) Save booking
+            _bookingRepository.AddBooking(booking);
+
+            // Return success with new BookingId
+            return (true, "Booking created successfully.", booking.BookingId);
+
+
+        }
 }
