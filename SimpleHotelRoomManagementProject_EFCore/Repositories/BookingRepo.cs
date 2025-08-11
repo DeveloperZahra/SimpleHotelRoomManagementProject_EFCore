55b8e657b5989e253b7f9b163a8699e7dc48272a
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 07:26:14 +0400

    Add a new Booking  to the database

--- SimpleHotelRoomManagementProject_EFCore/Repositories/BookingRepo.cs
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/BookingRepo.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleHotelRoomManagementProject_EFCore.Models;
 
 namespace SimpleHotelRoomManagementProject_EFCore.Repositories
 {
-    internal class BookingRepo
+    public  class BookingRepo
     {
+        private readonly HotelDbContext _context;
+        public BookingRepo(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // Add a new Booking  to the database
+        public void AddBooking(Booking  booking)
+        {
+            _context.Booking.Add(booking); // Adds the new Booking to the context
+            _context.SaveChanges(); // Saves changes to the database
+        }
+
     }
 }
