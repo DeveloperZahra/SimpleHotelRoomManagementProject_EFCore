8a0634ea7b5a637c43f582bd8e6575ad11e3d2bf
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 15:05:59 +0400

    // Configures the connection string for the SQL Server database     // This method is called by EF Core to set up the database connection

--- SimpleHotelRoomManagementProject_EFCore/HotelDbContext.cs
+++ SimpleHotelRoomManagementProject_EFCore/HotelDbContext.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimpleHotelRoomManagementProject_EFCore.Models;
+
 
 namespace SimpleHotelRoomManagementProject_EFCore
 {
-    internal class HotelDbContext
+    // HotelDbContext: Represents the session with the database for the Hotel Room Management System
+    // Inherits from Entity Framework Core's DbContext to manage database operations
+    public class HotelDbContext : DbContext
     {
+
+        // Configures the connection string for the SQL Server database
+        // This method is called by EF Core to set up the database connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-2EL6LAV ;Initial Catalog=HotelRoomManagementDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Booking> booking { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
     }
 }
