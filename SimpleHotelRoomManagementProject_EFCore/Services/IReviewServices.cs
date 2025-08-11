1abfa642509fd8e4462a82eee7d9601ddad992fc
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 13:32:37 +0400

    Retrieves a specific review based on its unique ID.

--- SimpleHotelRoomManagementProject_EFCore/Services/IReviewServices.cs
+++ SimpleHotelRoomManagementProject_EFCore/Services/IReviewServices.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+using System.Buffers.Text;
 using System.Xml;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -13,7 +14,7 @@ namespace SimpleHotelRoomManagementProject_EFCore.Services
         void AddNewReview(int ReviewId, int Rating, string Comment, DateTime ReviewDate, int BookingId, int GuestId); //Creates a new review record using provided details such as ID, rating, comment, date, booking, and guest.
         void DeleteReview(int reviewId); //Removes an existing review by its unique ID
         List<Review> GetAllReviews(); //Retrieves a list of all reviews in the system.
-        Review GetReviewById(int reviewId);
+        Review GetReviewById(int reviewId); //Retrieves a specific review based on its unique ID.
         void UpdateReview(int reviewId, int rating, string comments);
     }
 }
