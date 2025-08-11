010f8450d48ecebe278c120638f566ff7801c718
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 09:27:11 +0400

    // Interface defining the contract for room-related service operations. // Provides methods for adding, retrieving, updating, and removing rooms.

---
+++ SimpleHotelRoomManagementProject_EFCore/Services/IRoomServices.cs
@@ -0,0 +1,16 @@
+﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Services
+{
+    // Interface defining the contract for room-related service operations.
+    // Provides methods for adding, retrieving, updating, and removing rooms.
+    public interface IRoomServices
+    {
+        void AddNewRoom(decimal dailyRate, bool isAvailable);
+        List<Room> GetAllRooms();
+        Room GetRoomByID(int id);
+        void RemoveRoom(int id);
+        void UpdateRoom(int RoomId, bool isReserved);
+        void UpdateRoom(int RoomId, int daliyRate);
+    }
+}
