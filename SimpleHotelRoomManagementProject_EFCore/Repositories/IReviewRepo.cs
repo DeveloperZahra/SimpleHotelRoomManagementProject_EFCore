4020d9fd48598d8b9acf53ff0db8c257e57b35f3
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 09:40:48 +0400

    Adds a new Review entity to the database

--- SimpleHotelRoomManagementProject_EFCore/Repositories/IReviewRepo.cs
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/IReviewRepo.cs
@@ -6,7 +6,7 @@ namespace SimpleHotelRoomManagementProject_EFCore.Repositories
     // Classes implementing this interface must provide CRUD functionality for Review entities.
     public interface IReviewRepo
     {
-        void AddReview(Review review);
+        void AddReview(Review review); //Adds a new Review entity to the database
         void DeleteReview(int reviewId);
         List<Review> GetAllReviews();
         Review GetReviewById(int reviewId);
