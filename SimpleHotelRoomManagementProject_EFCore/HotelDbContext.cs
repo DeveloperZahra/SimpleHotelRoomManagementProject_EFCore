342bb2c70453961f49ed70aff29d16f6e897b392
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 15:07:16 +0400

    Represents the Booking table in the database

--- SimpleHotelRoomManagementProject_EFCore/HotelDbContext.cs
+++ SimpleHotelRoomManagementProject_EFCore/HotelDbContext.cs
@@ -23,7 +23,7 @@ namespace SimpleHotelRoomManagementProject_EFCore
 
 
         public DbSet<Room> Rooms { get; set; }   // Represents the Rooms table in the database
-        public DbSet<Booking> booking { get; set; }
+        public DbSet<Booking> booking { get; set; } //Represents the Booking table in the database
         public DbSet<Guest> Guests { get; set; }
         public DbSet<Review> Reviews { get; set; }
 
