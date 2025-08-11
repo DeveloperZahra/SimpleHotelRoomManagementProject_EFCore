23f5fb7421c6774dae56bb3215895c4823f1b447
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 09:22:41 +0400

    // Service class implementing IRoomServices interface. // Contains business logic related to managing rooms, // including adding, retrieving, updating, and removing rooms

--- SimpleHotelRoomManagementProject_EFCore/Services/RoomServices.cs
+++ SimpleHotelRoomManagementProject_EFCore/Services/RoomServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+using SimpleHotelRoomManagementProject_EFCore.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,69 @@ using System.Threading.Tasks;
 
 namespace SimpleHotelRoomManagementProject_EFCore.Services
 {
-    internal class RoomServices
+    // Service class implementing IRoomServices interface.
+    // Contains business logic related to managing rooms,
+    // including adding, retrieving, updating, and removing rooms
+    public class RoomServices : IRoomServices
     {
+        private readonly IRoomRepo _roomRepository;
+        public RoomServices(IRoomRepo roomRepository)
+        {
+            _roomRepository = roomRepository ?? throw new ArgumentNullException(nameof(roomRepository));
+        }
+
+
+        public void AddNewRoom(decimal dailyRate, bool isAvailable)
+        {
+            // Create a new Room object
+            var room = new Room
+            {
+                DailyRate = dailyRate,
+                IsReserved = isAvailable
+            };
+            _roomRepository.AddRoom(room);
+        }
+
+        // Get All Rooms in the database through the repository
+        public List<Room> GetAllRooms()
+        {
+            return _roomRepository.GetAllRooms();
+        }
+
+        // Get Room by id 
+        public Room GetRoomByID(int id)
+        {
+            return _roomRepository.GetRoomById(id);
+        }
+
+        // Remove Room
+        public void RemoveRoom(int id)
+        {
+            _roomRepository.DeleteRoom(id);
+        }
+
+        // update Reserved room
+        public void UpdateRoom(int RoomId, bool isReserved)
+        {
+            var existingroom = _roomRepository.GetRoomById(RoomId);
+            if (existingroom != null)
+            {
+                existingroom.IsReserved = isReserved;
+
+                _roomRepository.UpdateRoom(existingroom);
+            }
+        }
+
+        // update daliyRate of room 
+        public void UpdateRoom(int RoomId, int daliyRate)
+        {
+            var existingroom = _roomRepository.GetRoomById(RoomId);
+            if (existingroom != null)
+            {
+                existingroom.DailyRate = daliyRate;
+
+                _roomRepository.UpdateRoom(existingroom);
+            }
+        }
     }
 }
