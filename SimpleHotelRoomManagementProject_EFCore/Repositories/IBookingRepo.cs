47e57901964b9ee14dbbb61f563f69a90778d599
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 07:46:50 +0400

    Adds a new booking record to the database

--- SimpleHotelRoomManagementProject_EFCore/Repositories/IBookingRepo.cs
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/IBookingRepo.cs
@@ -7,7 +7,7 @@ namespace SimpleHotelRoomManagementProject_EFCore.Repositories
     // for adding, retrieving, updating, cancelling, and checking booking overlaps.
     public interface IBookingRepo
     {
-        void AddBooking(Booking booking);
+        void AddBooking(Booking booking); // Adds a new booking record to the database
         void CancelBooking(int BookingId);
         bool ExistsOverlap(int roomId, DateTime start, DateTime end);
         List<Booking> GetAllBooking();
