ddbd7098079ba9b0104d4e85748647ab10521e35
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 07:54:48 +0400

    // Interface that defines the contract for guest service operations. // Any class implementing this interface will handle business logic // related to adding, retrieving, updating, and deleting guest records.

--- SimpleHotelRoomManagementProject_EFCore/Services/IGuestServices.cs
+++ SimpleHotelRoomManagementProject_EFCore/Services/IGuestServices.cs
@@ -1,5 +1,9 @@
 ﻿namespace SimpleHotelRoomManagementProject_EFCore.Services
 {
+
+    // Interface that defines the contract for guest service operations.
+    // Any class implementing this interface will handle business logic
+    // related to adding, retrieving, updating, and deleting guest records.
     public interface IGuestServices
     {
         void AddNewGuest(int guestId, string guestname, string guestemail, string GuestPhoneNo);
