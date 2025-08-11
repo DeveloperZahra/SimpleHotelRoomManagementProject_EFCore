e3ae73fb54f9e229f3effc0b4dd714fc7d03c929
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 07:47:56 +0400

    Retrieves all bookings from the database

--- SimpleHotelRoomManagementProject_EFCore/Repositories/IBookingRepo.cs
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/IBookingRepo.cs
@@ -10,7 +10,7 @@ namespace SimpleHotelRoomManagementProject_EFCore.Repositories
         void AddBooking(Booking booking); // Adds a new booking record to the database
         void CancelBooking(int BookingId);  // Cancels a booking by its unique ID
         bool ExistsOverlap(int roomId, DateTime start, DateTime end);  // Checks if there is an overlapping booking for the same room within a given date range
-        List<Booking> GetAllBooking();
+        List<Booking> GetAllBooking();     // Retrieves all bookings from the database
         Booking GetBookingById(int BookingId);
         void UpdateBooking(Booking booking);
     }
