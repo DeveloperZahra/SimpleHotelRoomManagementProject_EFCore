7fdc186d665332ff25dcee63d610f2ede7ef7488
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 08:59:35 +0400

    Example enum for booking status (define outside this class)

---
+++ SimpleHotelRoomManagementProject_EFCore/Services/IBookingServices.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SimpleHotelRoomManagementProject_EFCore.Services
+{
+    public interface IBookingServices
+    {
+        (bool Ok, string Message, int? BookingId) AddNewBooking(int nights, DateTime checkInDate, int guestId, int roomId);
+    }
+}
