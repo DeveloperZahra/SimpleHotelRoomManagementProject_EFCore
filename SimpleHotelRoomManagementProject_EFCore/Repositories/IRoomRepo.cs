5aee47367138023149698c3a72a82406d064d59d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 09:18:16 +0400

    Interface defining contract for room repository operations. Classes implementing this interface must provide CRUD functionality for Room entities.

---
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/IRoomRepo.cs
@@ -0,0 +1,15 @@
+﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Repositories
+{
+    // Interface defining contract for room repository operations.
+    // Classes implementing this interface must provide CRUD functionality for Room entities.
+    public interface IRoomRepo
+    {
+        void AddRoom(Room room);
+        void DeleteRoom(int roomId);
+        List<Room> GetAllRooms();
+        Room GetRoomById(int roomId);
+        void UpdateRoom(Room room);
+    }
+}
