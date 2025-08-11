ca89f3c9d3704e6e3738ba2edc38716e09d3e9ce
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-11 07:45:44 +0400

    // Interface that defines the contract for booking repository operations. // Any class implementing this interface must provide concrete implementations // for adding, retrieving, updating, cancelling, and checking booking overlaps.

---
+++ SimpleHotelRoomManagementProject_EFCore/Repositories/IBookingRepo.cs
@@ -0,0 +1,17 @@
+﻿using SimpleHotelRoomManagementProject_EFCore.Models;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Repositories
+{
+    // Interface that defines the contract for booking repository operations.
+    // Any class implementing this interface must provide concrete implementations 
+    // for adding, retrieving, updating, cancelling, and checking booking overlaps.
+    public interface IBookingRepo
+    {
+        void AddBooking(Booking booking);
+        void CancelBooking(int BookingId);
+        bool ExistsOverlap(int roomId, DateTime start, DateTime end);
+        List<Booking> GetAllBooking();
+        Booking GetBookingById(int BookingId);
+        void UpdateBooking(Booking booking);
+    }
+}
