3e23eb3f2f0fafde499660125f19320f704bce9b
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-12 11:05:16 +0400

    // Simple console input validator utilities.     /// Keeps prompting until valid input is provided.

---
+++ SimpleHotelRoomManagementProject_EFCore/Validation/InputValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProject_EFCore.Validation
+{
+    // Simple console input validator utilities.
+    /// Keeps prompting until valid input is provided.
+    public static class InputValidator
+    {
+
+    }
+}
